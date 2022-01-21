0e9f5def6a7083dd46fe7066d27275776a1de66e
Gderu <orr.sharon.orr@gmail.com>
2022-01-21 22:15:34 +0200

    Program now works for basic hill climbing, except for a bug - when traveling to a point, the program stops just shy of it. This will be fixed in the next commit.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use rand::Rng;
 use rand::prelude::ThreadRng;
-use array_init;
 use std::ops::Range;
+use std::io;
 
-const NUM_SHAPES: usize = 1;
+const NUM_SHAPES: usize = 5;
 const MAP_WIDTH: f32 = 1000.;
 const MAP_HEIGHT: f32 = 1000.;
 const RADIUS_RANGE: Range<i32> = 100..200;
@@ -16,20 +16,58 @@ struct Line {
 }
 
 struct Map {
-    map: [Vec<Line>; NUM_SHAPES],
+    map: Vec<Line>,
     agent_pos: (f32, f32),
     goal_pos: (f32, f32),
-    rng: ThreadRng,
 }
 
 impl Map {
     fn init() -> Map {
         let mut rng = rand::thread_rng();
+        let map = (0..NUM_SHAPES).map(|_i| Map::create_shape(&mut rng)).collect::<Vec<Vec<Line>>>();
+        let agent_pos = Map::pick_point(&map, &mut rng);
+        let goal_pos = Map::pick_point(&map, &mut rng);
         Map {
-            map: array_init::array_init(|_i: usize| Map::create_shape(&mut rng)),
-            agent_pos: ((rng.gen::<f32>() - 0.5) * MAP_WIDTH, (rng.gen::<f32>() - 0.5) * MAP_HEIGHT),
-            goal_pos: ((rng.gen::<f32>() - 0.5) * MAP_WIDTH, (rng.gen::<f32>() - 0.5) * MAP_HEIGHT),
-            rng,
+            map: map.into_iter().flat_map(|l| l).collect(),
+            agent_pos,
+            goal_pos,
+        }
+    }
+
+    fn pick_point(map: &Vec<Vec<Line>>, rng: &mut ThreadRng) -> (f32, f32) {
+        let mut point;
+        let ray = (0., 1.0);
+        let mut reroll = false;
+        loop {
+            point = ((rng.gen::<f32>() - 0.5) * MAP_WIDTH, (rng.gen::<f32>() - 0.5) * MAP_HEIGHT);
+            for shape in map {
+                let mut count = 0;
+                for line in shape {
+                    if line.ray.0 * ray.1 - line.ray.1 * ray.0 == 0. {
+                        continue;
+                    }
+                    let t2 = (ray.0 * (line.origin.1 - point.1) + ray.1 * (point.0 - line.origin.0)) / (line.ray.0 * ray.1 - line.ray.1 * ray.0);
+                    let t1 = if ray.0 != 0. {
+                        (line.origin.0 + line.ray.0 * t2 - point.0) / ray.0
+                    } else if ray.1 != 0. {
+                        (line.origin.1 + line.ray.1 * t2 - point.1) / ray.1
+                    } else {
+                        continue;
+                    };
+                    if t1 >= 0. && t2 >= 0. && 1. >= t2 {//if the ray intersects with the line segment
+                        count += 1;
+                    }
+                }
+                if count % 2 == 1 { //the point is inside of the shape
+                    reroll = true;
+                    break;
+                }
+            }
+            if reroll {
+                reroll = false;
+                continue;
+            }
+            return point;
         }
     }
 
@@ -40,7 +78,6 @@ impl Map {
         let degree_offset = rng.gen::<f32>() * theta;
         let center_pos = ((rng.gen::<f32>() - 0.5) * MAP_WIDTH, (rng.gen::<f32>() - 0.5) * MAP_HEIGHT);
         let mut result = Vec::with_capacity(num_vertices);
-        println!("Rad: {}, theta: {}, offset: {}, center: {:?}", len_radius, theta, degree_offset, center_pos);
         let mut prev_point = (center_pos.0 + (len_radius * f32::cos(degree_offset + (num_vertices - 1) as f32 * theta)), center_pos.1 + (len_radius * f32::sin(degree_offset + (num_vertices - 1) as f32 * theta)));
         let mut new_point;
         for angle in (0..num_vertices).map(|i| degree_offset + i as f32 * theta) {
@@ -55,9 +92,64 @@ impl Map {
         result
     }
 
-    //gets the vertices that the agent can view from its current position
+    //gets points that the agent can see from its current position
     fn get_agent_view(&self) ->  Vec<(f32, f32)> {
-        unimplemented!();
+        let mut result = vec![];
+        self.find_closest_point(self.goal_pos, &mut result, false);
+        for point in self.map.iter().map(|line| line.origin) {
+            self.find_closest_point(point, &mut result, true);
+        }
+        result
+    }
+
+    //finds the closest point on a shape in the direction of variable point. Adds said point to result. Calls itself if the point found was a vertice with another point behind it.
+    fn find_closest_point(&self, point: (f32, f32), result: &mut Vec<(f32, f32)>, recursive: bool) {
+        let mut shortest_dist = -1.;
+        let mut closest_point_ray = None;
+        let ray = (point.0 - self.agent_pos.0, point.1 - self.agent_pos.1);
+
+        if point == self.goal_pos {//if the point is the goal, make the default value be the distance to it and it being the target. If there is a line between the goal and the current position, it will override this
+            shortest_dist = 1.;
+            closest_point_ray = Some((point, (0., 0.), (0., 0.), 0.));
+        }
+
+        for line in &self.map {
+            if line.ray.0 * ray.1 - line.ray.1 * ray.0 == 0. {
+                continue;
+            }
+            let t2 = (ray.0 * (line.origin.1 - self.agent_pos.1) + ray.1 * (self.agent_pos.0 - line.origin.0)) / (line.ray.0 * ray.1 - line.ray.1 * ray.0);
+            let t1 = if ray.0 != 0. {
+                (line.origin.0 + line.ray.0 * t2 - self.agent_pos.0) / ray.0
+            } else if ray.1 != 0. {
+                (line.origin.1 + line.ray.1 * t2 - self.agent_pos.1) / ray.1
+            } else {
+                continue;
+            };
+            if t1 >= 0. && t2 >= 0. && 1. >= t2 &&
+                (shortest_dist == -1. || shortest_dist > t1) {//if the point is on the line in the shape and is the closest point found so far
+
+                shortest_dist = t1;
+                closest_point_ray = Some(((self.agent_pos.0 + t1 * ray.0, self.agent_pos.1 + t1 * ray.1),
+                    line.origin,
+                    line.ray,
+                    t2,
+                ));
+            }
+        }
+        if let Some((closest_point, origin, ray, t2)) = closest_point_ray {
+            if t2 != 1. && t2 != 0. {
+                return;
+            }
+            result.push(closest_point);
+            if recursive {
+                return;
+            }
+            if closest_point == origin {
+                self.find_closest_point((origin.0 - ray.0 * 0.0001, origin.1 - ray.1 * 0.0001), result, false);
+            } else if closest_point == (origin.0 + ray.0, origin.1 + ray.1) {
+                self.find_closest_point((origin.0 + ray.0 * 1.0001, origin.1 + ray.1 * 1.0001), result, false);
+            }
+        }
     }
 
     fn get_agent_pos(&self) -> (f32, f32) {
@@ -68,12 +160,28 @@ impl Map {
         self.goal_pos
     }
 
-    /*Moves the agent to target.
+    /*Moves the agent to a point slightly before target.
     Target has to have been a position returned from get_agent_view - the function does not check its validity.
     Returns the distance to target.*/
     fn move_agent(&mut self, target: (f32, f32)) -> f32 {
-        let distance = ((self.agent_pos.0 - target.0).powf(2.) + (self.agent_pos.1 - target.1).powf(2.)).sqrt();
-        self.agent_pos = target;
+        let mut x = target.0 - self.agent_pos.0;
+        x = if x > 0. {
+            x * 0.9999
+        } else if x < 0. {
+            x * 1.0001
+        } else {
+            x
+        };
+        let mut y = target.1 - self.agent_pos.1;
+        y = if y > 0. {
+            y * 0.9999
+        } else if y < 0. {
+            y * 1.0001
+        } else {
+            y
+        };
+        let distance = (x.powf(2.) + y.powf(2.)).sqrt();
+        self.agent_pos = (self.agent_pos.0 + x, self.agent_pos.1 + y);
         return distance
     }
 
@@ -83,30 +191,50 @@ impl Map {
     }
 
     fn reached_goal(&self) -> bool {
-        self.goal_pos == self.agent_pos
+        dist(self.agent_pos, self.goal_pos) < 0.01
     }
 }
 
+fn dist(a: (f32, f32), b: (f32, f32)) -> f32 {
+    ((a.0 - b.0).powf(2.) + (a.1 - b.1).powf(2.)).sqrt()
+}
+
 fn hill_climbing_search(children: Vec<(f32, f32)>, curr_pos: (f32, f32), goal_pos: (f32, f32)) -> (f32, f32) {
-    unimplemented!();
+    let h_n = |point| dist(curr_pos, point) + dist(point, goal_pos) * 2.;
+    *children.iter()
+        .filter(|&&pos| dist(pos, curr_pos) >= 0.01)
+        .min_by(|&&a, &&b| h_n(a).partial_cmp(&h_n(b)).expect("No NaNs")).expect("Should never happenz")
+
 }
 
 fn main() {
     let mut map = Map::init();
+    println!("Shapes:");
+    for l in &map.map {
+        println!("\\operatorname{{polygon}}({:?}, {:?})", l.origin, l.target);
+    }
+    println!("Agent pos: {:?}", map.get_agent_pos());
+    println!("Goal pos: {:?}", map.get_goal_pos());
     let mut target;
     let mut score = 0.;
     let mut i = 0;
     let mut path = vec![map.get_agent_pos()];
+    let mut counter = 0;
     loop {
         target = hill_climbing_search(map.get_agent_view(), map.get_agent_pos(), map.get_goal_pos());
+        if target == map.get_goal_pos() && counter == 0 {
+            main();
+            return;
+        }
         score -= map.move_agent(target);
-        path.push(target);
+        println!("target: {:?}", map.get_agent_pos());
+
+        path.push(map.get_agent_pos());
+        io::stdin().read_line(&mut String::new());
         if map.reached_goal() {
-            for shape in map.map {
-                println!("Shape:");
-                for line in shape {
-                    println!("{:?}", line.origin);
-                }
+            println!("IN");
+            for line in &map.map {
+                println!("{:?}, {:?}", line.origin, line.target);
             }
             println!("Path:");
             for point in path {
@@ -120,5 +248,6 @@ fn main() {
         if i == 1 {
             break;
         }
+        counter += 1;
     }
 }
